a20b65021361ccd484148826858a51604ed7bb00
valux <valux@qq.com>
2015-04-28 17:20:03 +0800

    init

---
+++ MarkdownAddin/FormRegionMarkdown.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace MarkdownAddin
+{
+    [System.ComponentModel.ToolboxItemAttribute(false)]
+    partial class FormRegionMarkdown : Microsoft.Office.Tools.Outlook.FormRegionBase
+    {
+        public FormRegionMarkdown(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            : base(Globals.Factory, formRegion)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.rtbMkdEditor = new System.Windows.Forms.RichTextBox();
+            this.wbMkdBrowser = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(735, 568);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(735, 593);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.rtbMkdEditor);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.wbMkdBrowser);
+            this.splitContainer1.Size = new System.Drawing.Size(735, 568);
+            this.splitContainer1.SplitterDistance = 349;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // rtbMkdEditor
+            // 
+            this.rtbMkdEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbMkdEditor.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbMkdEditor.Location = new System.Drawing.Point(0, 0);
+            this.rtbMkdEditor.Name = "rtbMkdEditor";
+            this.rtbMkdEditor.Size = new System.Drawing.Size(349, 568);
+            this.rtbMkdEditor.TabIndex = 0;
+            this.rtbMkdEditor.Text = "";
+            this.rtbMkdEditor.TextChanged += new System.EventHandler(this.rtbMkdEditor_TextChanged);
+            // 
+            // wbMkdBrowser
+            // 
+            this.wbMkdBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbMkdBrowser.Location = new System.Drawing.Point(0, 0);
+            this.wbMkdBrowser.Margin = new System.Windows.Forms.Padding(10);
+            this.wbMkdBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbMkdBrowser.Name = "wbMkdBrowser";
+            this.wbMkdBrowser.Size = new System.Drawing.Size(382, 568);
+            this.wbMkdBrowser.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(111, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // FormRegionMarkdown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "FormRegionMarkdown";
+            this.Size = new System.Drawing.Size(735, 593);
+            this.FormRegionShowing += new System.EventHandler(this.FormRegionMarkdown_FormRegionShowing);
+            this.FormRegionClosed += new System.EventHandler(this.FormRegionMarkdown_FormRegionClosed);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        #region Form Region Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private static void InitializeManifest(Microsoft.Office.Tools.Outlook.FormRegionManifest manifest, Microsoft.Office.Tools.Outlook.Factory factory)
+        {
+            manifest.FormRegionName = "Markdown";
+            manifest.ShowReadingPane = false;
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox rtbMkdEditor;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.WebBrowser wbMkdBrowser;
+
+        public partial class FormRegionMarkdownFactory : Microsoft.Office.Tools.Outlook.IFormRegionFactory
+        {
+            public event Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler FormRegionInitializing;
+
+            private Microsoft.Office.Tools.Outlook.FormRegionManifest _Manifest;
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public FormRegionMarkdownFactory()
+            {
+                this._Manifest = Globals.Factory.CreateFormRegionManifest();
+                FormRegionMarkdown.InitializeManifest(this._Manifest, Globals.Factory);
+                this.FormRegionInitializing += new Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler(this.FormRegionMarkdownFactory_FormRegionInitializing);
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public Microsoft.Office.Tools.Outlook.FormRegionManifest Manifest
+            {
+                get
+                {
+                    return this._Manifest;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.IFormRegion Microsoft.Office.Tools.Outlook.IFormRegionFactory.CreateFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            {
+                FormRegionMarkdown form = new FormRegionMarkdown(formRegion);
+                form.Factory = this;
+                return form;
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            byte[] Microsoft.Office.Tools.Outlook.IFormRegionFactory.GetFormRegionStorage(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                throw new System.NotSupportedException();
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            bool Microsoft.Office.Tools.Outlook.IFormRegionFactory.IsDisplayedForItem(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                if (this.FormRegionInitializing != null)
+                {
+                    Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs cancelArgs = Globals.Factory.CreateFormRegionInitializingEventArgs(outlookItem, formRegionMode, formRegionSize, false);
+                    this.FormRegionInitializing(this, cancelArgs);
+                    return !cancelArgs.Cancel;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.FormRegionKindConstants Microsoft.Office.Tools.Outlook.IFormRegionFactory.Kind
+            {
+                get
+                {
+                    return Microsoft.Office.Tools.Outlook.FormRegionKindConstants.WindowsForms;
+                }
+            }
+        }
+    }
+
+    partial class WindowFormRegionCollection
+    {
+        internal FormRegionMarkdown FormRegionMarkdown
+        {
+            get
+            {
+                foreach (var item in this)
+                {
+                    if (item.GetType() == typeof(FormRegionMarkdown))
+                        return (FormRegionMarkdown)item;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ MarkdownAddin/FormRegionMarkdown.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace MarkdownAddin
+{
+    partial class FormRegionMarkdown
+    {
+        #region Form Region Factory
+
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Note)]
+        [Microsoft.Office.Tools.Outlook.FormRegionName("MarkdownAddin.FormRegionMarkdown")]
+        public partial class FormRegionMarkdownFactory
+        {
+            // Occurs before the form region is initialized.
+            // To prevent the form region from appearing, set e.Cancel to true.
+            // Use e.OutlookItem to get a reference to the current Outlook item.
+            private void FormRegionMarkdownFactory_FormRegionInitializing(object sender, Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs e)
+            {
+            }
+        }
+
+        #endregion
+
+        private string tmpl = string.Empty;
+        // Occurs before the form region is displayed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void FormRegionMarkdown_FormRegionShowing(object sender, System.EventArgs e)
+        {
+            var reader = new StreamReader(@"D:\Workspace\MarkdownAddin\MarkdownAddin\markdown\markdown.html");
+            tmpl = reader.ReadToEnd();
+            reader.Close();
+
+            reader = new StreamReader(@"D:\Workspace\MarkdownAddin\MarkdownAddin\markdown\bootstrap.min.css");
+            tmpl = tmpl.Replace("#TMPL_STYLE_MARKDOWN#", reader.ReadToEnd());
+            reader.Close();
+            reader = new StreamReader(@"D:\Workspace\MarkdownAddin\MarkdownAddin\markdown\styles\monokai_sublime.css");
+            tmpl = tmpl.Replace("#TMPL_STYLE_HIGHLIGHE#", reader.ReadToEnd());
+            reader.Close();
+
+
+            wbMkdBrowser.Navigate("about:blank");
+        }
+
+        // Occurs when the form region is closed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void FormRegionMarkdown_FormRegionClosed(object sender, System.EventArgs e)
+        {
+        }
+
+        private void rtbMkdEditor_TextChanged(object sender, EventArgs e)
+        {
+            var html = CommonMark.CommonMarkConverter.Convert(rtbMkdEditor.Text);
+            wbMkdBrowser.DocumentText = tmpl.Replace("#TMPL_CONTENT#", html);
+
+
+            var mail = Globals.ThisAddIn.Application.ActiveInspector().CurrentItem as Outlook.MailItem;
+            mail.HTMLBody = wbMkdBrowser.DocumentText;
+        }
+    }
+}
